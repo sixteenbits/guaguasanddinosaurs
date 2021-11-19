5e11a1e3c791ff9565e33e8459715f2cd2a03622
zerasul <zerasul@gmail.com>
2021-11-19 20:39:23 +0100

    initial commit

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/**
+ * Hello World Example
+ * Created With Genesis-Code extension for Visual Studio Code
+ * Use "Genesis Code: Compile" command to compile this program.
+ **/
+#include <genesis.h>
+
+int main()
+{
+    VDP_drawText("Hello Sega!!", 10,13);
+    while(1)
+    {
+        //For versions prior to SGDK 1.60 use VDP_waitVSync instead.
+        VDP_waitVSync();
+    }
+    return (0);
+}
