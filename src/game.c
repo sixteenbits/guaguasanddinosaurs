2c88d79d401826325a73432d41513d7edf75fe97
zerasul <zerasul@gmail.com>
2021-11-20 12:24:51 +0100

    adding some changes for initialization systems

--- src/game.c
+++ src/game.c
@@ -10,11 +10,15 @@ void initGame(){
     game.current_state=LOGO_STATE;
     game.initiated=0;
     game.tics=0;
+    SYS_disableInts();
     VDP_setScreenWidth320();
+    VDP_setScreenHeight224();
+    JOY_init();
+    JOY_setEventHandler(handleAsyncInput);
     SPR_init();
+    SYS_enableInts();
     
-    
-    Player player = {0,NULL,16,16,0,0};
+    Player player = {0,NULL,POWER_1,16,16,0};
     game.player=player;
 }
 void initState(){
@@ -36,6 +40,9 @@ void initState(){
             game.player.x=160;
             game.player.y=160;
             VDP_drawImageEx(BG_B,&stage1,TILE_ATTR_FULL(PAL0,FALSE,FALSE,FALSE,game.ind),0,0,TRUE,CPU);
+            VDP_setPalette(PAL2,player_sprite.palette->data);
+            game.player.sprite= SPR_addSprite(&player_sprite, game.player.x,game.player.y,TILE_ATTR(PAL2,FALSE,FALSE,FALSE));
+            SPR_setAnim(game.player.sprite,4);
             break;
         }
         game.initiated=1;
@@ -74,6 +81,10 @@ void handleSincInput(){
     
 
     
+}
+
+void handleAsyncInput(u16 joy, u16 state, u16 change){
+
 }
 
 void updatePhisycs(){
