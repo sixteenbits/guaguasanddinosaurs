0811f0286f5493cee87237a76f4990926298f7b7
emiliollbb <emiliollbb@gmail.com>
2021-11-20 11:57:10 +0100

    Player jump

--- inc/game.h
+++ inc/game.h
@@ -1,12 +1,20 @@
 #ifndef _GAME_H_
 #define _GAME_H_
 #include <genesis.h>
+// Player struct
 typedef struct{
+    // Player lives
     u8 lives;
+    // Player sprite
     Sprite * sprite;
     u8 state;
+    // Player pos X
     u16 x;
+    // Player pos Y
     u16 y;
+    // Jump Speed
+    u16 vy;
+    // Player coins
     u16 varacoins;
 }Player;
 
